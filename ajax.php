b1bc96ce008590984921d28ad1fe7e9df17e53d6
Steven Nguyen <steven.nguyen@qsoftvietnam.com>
2015-11-19 14:19:23 +0700

    fix bug

--- ajax.php
+++ ajax.php
@@ -2,7 +2,7 @@
 // Get all members from JSON file
 $members = json_decode(file_get_contents('data/data.json'))->members;
 // Get a random key of the array
-$randomKey = rand(0, count($members));
+$randomKey = rand(0, count($members) - 1);
 // We update the statistics
 $members[$randomKey]->times++;
 // And the put updated info into JSON file
